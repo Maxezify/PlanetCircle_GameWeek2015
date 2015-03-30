d4c5c7761159c339fc8993feda3031bdeb3f35ad
Maxezify <gillet.maxence.isart@gmail.com>
2015-03-30 17:32:38 +0200

    Add Input Controller for PlayerBoards

---
+++ Assets/PlayerBoardsManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBoardsManager : MonoBehaviour {
+
+	private float rotSpeed = 20f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		InputManager();
+
+	
+	}
+
+	void InputManager() {
+
+		if (Input.GetKeyDown ("space"))	{
+
+			Debug.Log("Je passe");
+			transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+
+		}
+
+	}
+}
---
+++ Assets/Scripts/SphereManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereManager : MonoBehaviour {
+
+	private float rotSpeed = 75f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		InputManager();
+	
+	}
+
+	void InputManager() {
+		
+		if (Input.GetKey(KeyCode.RightArrow))	{
+			
+			Debug.Log("Je passe");
+			transform.Rotate(0, 0, rotSpeed * Time.deltaTime);
+			
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))	{
+			
+			Debug.Log("Je passe");
+			transform.Rotate(0, 0, -rotSpeed * Time.deltaTime);
+			
+		}
+
+}
+}
