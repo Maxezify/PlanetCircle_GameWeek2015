5478e47a8260575c395de27a4106049e81eb8ee0
Maxezify <gillet.maxence.isart@gmail.com>
2015-03-30 15:13:17 +0200

    Add good transform.rotate for earth !

---
+++ Assets/EarthManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthManager : MonoBehaviour {
+
+	float rotSpeed = 10;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		EarthMovement();
+	
+	}
+
+	void EarthMovement() {
+
+		transform.Rotate(0, 0, rotSpeed * Time.deltaTime, Space.World);
+
+
+	}
+}
