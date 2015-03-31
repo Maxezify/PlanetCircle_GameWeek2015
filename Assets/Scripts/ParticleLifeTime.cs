a833fd022fc3bac06905a5c17cb2bc4162ec6fb5
TagliatL <maurer.paul@ymail.com>
2015-03-31 11:11:26 +0200

    change on bulletmanager

---
+++ Assets/Scripts/ParticleLifeTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleLifeTime : MonoBehaviour {
+
+	public float lifetime;
+	float timeBeforeDeath;
+
+
+
+	void Update() {
+		timeBeforeDeath++;
+
+		if(timeBeforeDeath >= lifetime)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
