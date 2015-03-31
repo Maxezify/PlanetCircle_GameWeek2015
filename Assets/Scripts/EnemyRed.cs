72526c1360ba26f92c5dea09ea68ca1411632ddc
Mazelpomme <c.husser@student.isartdigital.com>
2015-03-31 11:36:23 +0200

    classe mere enemy v2

---
+++ Assets/Scripts/EnemyRed.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRed : Enemy {
+	public int timer = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer == attackSpeed) {
+			Fire ();
+			timer = 0;
+		} else {
+			timer ++;
+		}
+	}
+
+	public override void Fire ()
+	{
+		base.Fire ();
+		Debug.Log ("fire");
+		Vector3 posis = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+		
+		Instantiate(shoot, posis, transform.rotation);
+		
+	}
+}
