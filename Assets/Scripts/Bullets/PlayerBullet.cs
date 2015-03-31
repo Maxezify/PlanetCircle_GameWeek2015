45b3f8ea7a40f0c15e1ca8d87489cb8fa00fbe1c
TagliatL <maurer.paul@ymail.com>
2015-03-31 17:18:20 +0200

    je suis muuuuuusic

---
+++ Assets/Scripts/Bullets/PlayerBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PlayerBullet : BulletManager {
+
+	protected override void Start() {
+		base.Start();
+		StartInstanciated.transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+	protected override void OnTriggerEnter (Collider other) {
+		base.OnTriggerEnter (other);
+		if (other.tag == "Enemy")  {
+			other.gameObject.GetComponent<Enemy>().TakeDamage(damages);
+		}
+		if (other.tag == "Earth")  {
+			other.gameObject.GetComponent<EarthManager>().TakeDamage(damages);
+		}
+
+	}
+}
