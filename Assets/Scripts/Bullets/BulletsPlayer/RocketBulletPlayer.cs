00c484e5ba2bbad5959f9c169e60cbde6070511b
TagliatL <maurer.paul@ymail.com>
2015-04-01 23:33:01 +0200

    added some cool things WIP

--- Assets/Scripts/Bullets/BulletsPlayer/RocketBulletPlayer.cs
+++ Assets/Scripts/Bullets/BulletsPlayer/RocketBulletPlayer.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class RocketBulletPlayer : PlayerBullet {
 
+	Vector2 derivesXY;
 	// Use this for initialization
 	void Start () {
-	
+		derivesXY = new Vector2(Random.Range(-2.0f,2.0f), Random.Range(-2.0f,2.0f));
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	protected override void Update () {
+		ProjectileSpeed += 1;
+		if(ProjectileSpeed <= 10) {
+			transform.Translate (Vector3.left * Time.deltaTime * derivesXY [0]);
+			transform.Translate (Vector3.up * Time.deltaTime * derivesXY [1]);
+		} else if(ProjectileSpeed > 100) {
+			Destroy(gameObject);
+		} else {
+			transform.Translate(Vector3.up * ProjectileSpeed/4 * Time.deltaTime);
+		}
 	}
 }
---
+++ Assets/Scripts/Other/DetectPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPlayer : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player") {
+			transform.root.GetComponent<RocketFoe>().targetPlayer = other.gameObject;
+		}
+	}
+}
