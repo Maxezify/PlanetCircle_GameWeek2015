3fc105a9519420919d47b2816e98f35b75d4a72f
Maxezify <gillet.maxence.isart@gmail.com>
2015-04-01 12:22:56 +0200

    Add Heart in SpaceBoards

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	Text Score;
+	private int life;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Score = GetComponent<Text>();
+		life = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBoardsManager>().life;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+		
+		
+	}
