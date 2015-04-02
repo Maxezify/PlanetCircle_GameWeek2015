428e8b68d572f159d500f91d05e0b26d3eeea6b0
TagliatL <maurer.paul@ymail.com>
2015-04-02 12:06:24 +0200

    EXPLOSION DU GHETTO

---
+++ Assets/Scripts/ResizeEffect.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResizeEffect : MonoBehaviour {
+
+	public float delay;
+	float timer;
+	void Update() {
+		timer += 0.1f;
+		if(timer >= delay){
+			transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+		}
+	}
+}
