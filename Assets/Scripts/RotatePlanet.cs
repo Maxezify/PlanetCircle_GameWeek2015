3df4d5ad4b8110cc1ee4e1f3ce27e34d9fe0ca5f
TagliatL <maurer.paul@ymail.com>
2015-04-03 12:31:56 +0200

    équilibrage des ennemis

---
+++ Assets/Scripts/RotatePlanet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatePlanet : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 5* Time.deltaTime, 0, Space.World);
+	}
+}
