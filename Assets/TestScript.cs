add2cff50308d22522f26fc366568816ab2f4caa
Maxezify <gillet.maxence.isart@gmail.com>
2015-04-03 04:58:56 +0200

    Avancement sur le menu, selection avec le click

---
+++ Assets/TestScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TestScript : MonoBehaviour {
+
+	public void Start() {
+		//EventSystem.current.SetSelectedGameObject(totoG);
+		//EventSystem.current.currentSelectedGameObject;
+		//EventSystem.current.lastSelectedGameObject;
+	}
+
+	public void toto(int i)
+	{
+		Application.LoadLevel("Gameplay-Clean");
+	}
+}
